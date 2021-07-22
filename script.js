c46d2fdb8181c97648479cfa91ad5e23ae0670c3
Anshuman Gupta <68121457+Anshuman4492@users.noreply.github.com>
2021-07-22 19:42:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const codeforces = document.getElementById('codeforces');
+const codechef = document.getElementById('codechef');
+const leetcode = document.getElementById('leetcode');
+const atcoder = document.getElementById('atcoder');
+const gfg = document.getElementById('gfg');
+
+codeforces.addEventListener('click', () => {
+    window.location.href = "http://www.codeforces.com";
+})
+codechef.addEventListener('click', () => {
+    window.location.href = "http://www.codechef.com";
+})
+leetcode.addEventListener('click', () => {
+    window.location.href = "http://www.leetcode.com";
+})
+atcoder.addEventListener('click', () => {
+    window.location.href = "http://www.atcoder.jp";
+})
+gfg.addEventListener('click', () => {
+    window.location.href = "http://www.geeksforgeeks.org";
+})
