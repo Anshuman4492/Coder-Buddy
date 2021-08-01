afe7960201e8c0a35b8fa9c84720a07ba5fdd8b4
Anshuman Gupta <68121457+Anshuman4492@users.noreply.github.com>
2021-08-01 09:43:15 +0530

    new commit of bootstrap

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ const codechef = document.getElementById('codechef');
 const leetcode = document.getElementById('leetcode');
 const atcoder = document.getElementById('atcoder');
 const gfg = document.getElementById('gfg');
-
+const signup = document.getElementById('signup');
+const login = document.getElementById('login');
 codeforces.addEventListener('click', () => {
     window.location.href = "http://www.codeforces.com";
 })
@@ -19,3 +20,46 @@ atcoder.addEventListener('click', () => {
 gfg.addEventListener('click', () => {
     window.location.href = "http://www.geeksforgeeks.org";
 })
+
+// SignUp and Login Button
+const signup = document.getElementById('signup');
+const login = document.getElementById('login');
+signup.addEventListener('click', () => {
+    window.location.href = "../signup.html";
+});
+// Specific Site Contests List
+
+const fsite = 'codeforces';
+const codeforcessite = document.getElementById('sitelist');
+
+console.log(codeforcessite);
+
+
+// Default List
+const contests_api_url = "https://clist.by:443/api/v2/contest/all";
+var contestsList;
+// Defining async function
+fetch(`https://kontests.net/api/v1/${fsite}`)
+    .then(res => {
+        return res.json();
+    })
+    .then(data => {
+        // console.log(data);
+        show(data);
+    })
+    .catch(error => console.log(`Error is ${error}`));
+
+function show(data) {
+    let tab = '';
+    // Loop to access all rows 
+    for (let i = 0; i < data.length; i++) {
+        tab += `<tr> 
+                    <td>${data[i].name} </td>
+                    <td><a href='${data[i].url}'></a></td>
+                    <td>${data[i].start_time}</td>
+                    <td>${data[i].end_time}</td>
+                </tr>`;
+    }
+    // Setting innerHTML as tab variable
+    document.getElementById("contests").innerHTML = tab;
+}
